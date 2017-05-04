0a1481b0b35032294967e979f728f2e51a889439
Norvin <ngydev@outlook.com>
2017-05-04 14:07:07 +0800

    Add short comments

--- src/IndexSubscriber.php
+++ src/IndexSubscriber.php
@@ -6,6 +6,11 @@ use Source\CustomEvent;
 
 class IndexSubscriber implements EventSubscriberInterface
 {
+    /**
+    * Returns list of events this subscriber is subscribed to.
+    * @param: none
+    * @return: array
+    **/
     public static function getSubscribedEvents()
     {
         return array(
@@ -13,11 +18,17 @@ class IndexSubscriber implements EventSubscriberInterface
         );
     }
 
+
+    /**
+    * Prints the time everytime it is triggered.
+    * @param: CustomEvent
+    * @return: none
+    *
+    **/
     public function onIndexAction(CustomEvent $event)
     {
         $date = new \DateTime();
         echo "Event triggered at: " . $date->format('Y-m-d H:i:s') . "\n";
     }
-
 
 }
